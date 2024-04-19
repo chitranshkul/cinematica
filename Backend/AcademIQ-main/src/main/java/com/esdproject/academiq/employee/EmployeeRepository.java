248df127e39e34816a6ba841e7c02bebb64b01b6
GramyaGupta <112greatannie@gmail.com>
2024-04-19 12:35:13 +0530

    Tables Added

--- Backend/AcademIQ-main/src/main/java/com/esdproject/academiq/employee/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.esdproject.academiq.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
