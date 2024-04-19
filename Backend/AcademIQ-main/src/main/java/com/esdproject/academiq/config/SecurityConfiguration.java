d9ec0a14ebc23082ba6f4ee8bf41bac0837d2aa1
yashswi <yashswi2401iit@gmail.com>
2024-04-19 15:50:18 +0530

    Upload-files-done

--- Backend/AcademIQ-main/src/main/java/com/esdproject/academiq/config/SecurityConfiguration.java
+++ Backend/AcademIQ-main/src/main/java/com/esdproject/academiq/config/SecurityConfiguration.java
@@ -35,8 +35,10 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
+    private static final String[] WHITE_LIST_URL = {
+            "/api/v1/auth/**",
             "/v2/api-docs",
+            "/api/v1/**",
             "/v3/api-docs",
             "/v3/api-docs/**",
             "/swagger-resources",
---
+++ Backend/AcademIQ-main/src/main/java/com/esdproject/academiq/service/StorageService.java
@@ -0,0 +1,48 @@
+package com.esdproject.academiq.service;
+
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Service
+@Slf4j
+public class StorageService {
+
+    @Value("${application.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    public String uploadFile(MultipartFile file) {
+        File fileObj = convertMultiPartFileToFile(file);
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
+        fileObj.delete(); //delete from the s3 bucket
+        return "File uploaded : " + fileName;
+    }
+
+    private File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            log.error("Error converting multipartFile to file", e);
+        }
+        return convertedFile;
+    }
+}
+
+
