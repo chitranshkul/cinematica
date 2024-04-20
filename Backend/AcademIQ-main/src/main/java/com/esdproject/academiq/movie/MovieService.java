b2442987a85a549bca015908d6fe0a3e26947511
yashswi <yashswi2401iit@gmail.com>
2024-04-20 16:45:24 +0530

    merged with main

--- Backend/AcademIQ-main/src/main/java/com/esdproject/academiq/movie/MovieService.java
+++ Backend/AcademIQ-main/src/main/java/com/esdproject/academiq/movie/MovieService.java
@@ -1,4 +1,45 @@
 package com.esdproject.academiq.movie;
 
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Service
+@Slf4j
 public class MovieService {
+
+    @Value("${application.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    public String uploadFile(MultipartFile file) {
+        File fileObj = convertMultiPartFileToFile(file);
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
+        fileObj.delete(); //delete from the s3 bucket
+        return "File uploaded : " + fileName;
+    }
+
+    private File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            log.error("Error converting multipartFile to file", e);
+        }
+        return convertedFile;
+    }
 }
+
+
