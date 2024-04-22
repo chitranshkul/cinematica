2b178f79575a5a1c7d3a1ba2423da2dfbaef1a52
codaks <found.code.7@gmail.com>
2024-04-22 05:49:53 +0530

    Repo File Merged

--- Backend/AcademIQ-main/src/main/java/com/esdproject/academiq/movie/MovieRepository.java
+++ Backend/AcademIQ-main/src/main/java/com/esdproject/academiq/movie/MovieRepository.java
@@ -1,9 +1,15 @@
 package com.esdproject.academiq.movie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
-    Optional<Movie> findById(Movie movieId);
+    Optional<List<Movie>> findByGenre(String genre);
+    Optional<List<Movie>> findByTitle(String title);
+    Optional<List<Movie>> findByLanguage(String language);
 }
