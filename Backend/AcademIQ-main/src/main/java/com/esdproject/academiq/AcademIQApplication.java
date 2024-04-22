cc81c089a27c73db5d91a5bc57693375ecf19345
codaks <found.code.7@gmail.com>
2024-04-22 21:40:45 +0530

    Logs Completed

--- Backend/AcademIQ-main/src/main/java/com/esdproject/academiq/AcademIQApplication.java
+++ Backend/AcademIQ-main/src/main/java/com/esdproject/academiq/AcademIQApplication.java
@@ -12,20 +12,30 @@ import com.esdproject.academiq.user.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
-public class AcademIQApplication {
+public class AcademIQApplication extends SpringBootServletInitializer {
 
-//	private static final Logger LOGGER = LogManager.getLogger(Application.class);
+	private static final Logger LOGGER = LogManager.getLogger(Application.class);
 	public static void main(String[] args) {
 		SpringApplication.run(AcademIQApplication.class, args);
+		LOGGER.info("Info level log message");
+		LOGGER.debug("Debug level log message");
+		LOGGER.error("Error level log message");
 	}
+
+
+
+
 
 	@Bean
 	public CommandLineRunner commandLineRunner(
@@ -38,7 +48,7 @@ public class AcademIQApplication {
 
 			try {
 
-			
+
 
 
 			}
